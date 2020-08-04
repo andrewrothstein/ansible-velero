d1a71daea18d8f9a1569d8a5982d273e67ff7401
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-08-03 20:59:05 -0400

    velero:1.4.2

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,36 +1,41 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e
 readonly DIR=~/Downloads
 readonly MIRROR=https://github.com/vmware-tanzu/velero/releases/download
 
+# https://github.com/vmware-tanzu/velero/releases/download/v1.4.2/CHECKSUM
+
 dl()
 {
-    local ver=$1
-    local os=$2
-    local arch=$3
+    local lchecksum=$1
+    local ver=$2
+    local os=$3
+    local arch=$4
     local platform=${os}-${arch}
     local file=velero-${ver}-${platform}.tar.gz
     local url=$MIRROR/$ver/$file
-    local lfile=$DIR/$file
-
-    if [ ! -e $lfile ];
-    then
-        wget -q -O $lfile $url
-    fi
-
     printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+    printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksum | awk '{print $1}')
 }
 
 dl_ver () {
     local ver=$1
+    local checksum_url=${MIRROR}/${ver}/CHECKSUM
+    printf "  # %s\n" $checksum_url
+    local lchecksum=$DIR/velero-${ver}-checksums.txt
+
+    if [ ! -e $lchecksum ];
+    then
+        wget -q -O $lchecksum $checksum_url
+    fi
+
     printf "  %s:\n" $ver
-    dl $ver darwin amd64
-    dl $ver linux amd64
-    dl $ver linux arm
-    dl $ver linux arm64
-    dl $ver windows amd64
+    dl $lchecksum $ver darwin amd64
+    dl $lchecksum $ver linux amd64
+    dl $lchecksum $ver linux arm
+    dl $lchecksum $ver linux arm64
+    dl $lchecksum $ver windows amd64
 }
 
 
-dl_ver ${1:-v1.4.0}
+dl_ver ${1:-v1.4.2}
